1611d40ccb6e831a225e0f273e4a071a9a7b22f8
Dmitry Cherkasov <www.demontools999@mail.ru>
2024-06-12 20:54:18 +0400

    Исправление ошибки римские+арабские числа (0+V)

--- src/main/java/ru/cherkasov/Roman.java
+++ src/main/java/ru/cherkasov/Roman.java
@@ -1,7 +1,7 @@
 package ru.cherkasov;
 
 public class Roman {
-    static String[] romanArray = new String[]{"0", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX", "X", "XI",
+    static String[] romanArray = new String[]{"I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX", "X", "XI",
             "XII", "XIII", "XIV", "XV", "XVI", "XVII", "XVIII", "XIX", "XX", "XXI", "XXII", "XXIII", "XXIV",
             "XXV", "XXVI", "XXVII", "XXVIII", "XXIX", "XXX", "XXXI", "XXXII", "XXXIII", "XXXIV", "XXXV", "XXXVI",
             "XXXVII", "XXXVIII", "XXXIX", "XL", "XLI", "XLII", "XLIII", "XLIV", "XLV", "XLVI", "XLVII", "XLVIII",
